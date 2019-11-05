6beee767e55ca74e62ebcc85927206b2aa450e0b
masknugget <54693275+masknugget@users.noreply.github.com>
2019-11-05 14:58:59 +0800

    Update __init__.py

--- django/__init__.py
+++ django/__init__.py
@@ -2,12 +2,12 @@ from django.utils.version import get_version
 
 VERSION = (3, 1, 0, 'alpha', 0)
 
-__version__ = get_version(VERSION)
+__version__ = get_version(VERSION)                                                            # --- 原来version--- 是写在这里的
+                                                                                              # --- __all__ = [] 这个也写在这里
 
-
-def setup(set_prefix=True):
+def setup(set_prefix=True):                                                                   # 建造者模式将所有写的类进行输出，在java
     """
-    Configure the settings (this happens as a side effect of accessing the
+    Configure the settings (this happens as a side effect of accessing the                    # 可能是个接口之类的
     first setting), configure logging and populate the app registry.
     Set the thread-local urlresolvers script prefix if `set_prefix` is True.
     """
